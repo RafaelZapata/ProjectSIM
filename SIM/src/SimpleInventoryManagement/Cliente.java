145fb3908f9523d3085ddb537bafcc5f80d21506
diogoasp <diogoasp13@gmail.com>
2019-05-30 15:24:36 -0300

    Primeiro Commit

--- SIM/src/SimpleInventoryManagement/Cliente.java
+++ SIM/src/SimpleInventoryManagement/Cliente.java
@@ -1,10 +1,16 @@
 package SimpleInventoryManagement;
 
+import java.util.Date;
+
 public class Cliente implements iCadastrar{
 	
 	private String nome;
 	private String endereco;
 	private String telefone;
+	private String cpf;
+	private Date dataNascimento;
+	private String genero;
+	
 	
 	public String getNome() {
 		return nome;
@@ -27,6 +33,24 @@ public class Cliente implements iCadastrar{
 		this.telefone = telefone;
 	}
 	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 	@Override
 	public void novo_cadastro() {
 		this.setNome(nome);
