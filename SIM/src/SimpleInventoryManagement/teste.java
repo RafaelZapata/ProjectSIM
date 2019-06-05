9f6b480f0159daa6c3e7bfcb19293122dd1848fe
Rafael Zapata <faelzapata2011@gmail.com>
2019-06-05 13:43:37 -0300

    Criada a conexão com o banco de dados

--- SIM/src/SimpleInventoryManagement/teste.java
+++ SIM/src/SimpleInventoryManagement/teste.java
@@ -1,12 +1,17 @@
 package SimpleInventoryManagement;
 
+import Util.DatabaseConnect;
+
 public class teste {
 	
 	public static void main(String[] args) {
 		
+		DatabaseConnect Connect = new DatabaseConnect();
 //		Declaração de variaveis e objetos
 //		Produto[] Lista = new Produto[3];
 		Vendedor p1 = new Vendedor();
 		p1.novo_cadastro();
+		
+		Connect.connection();
 	}
 }
